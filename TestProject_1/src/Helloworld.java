c4ace2ed58eca5f58d8cd7d45e14f348c3b46c69
knawate <nawate.k@gmail.com>
2018-06-17 07:48:28 +0530

    Added one sum method (04SaiNawate)

--- TestProject_1/src/Helloworld.java
+++ TestProject_1/src/Helloworld.java
@@ -4,6 +4,12 @@ public class Helloworld {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World");
+		sum();
+	}
+	
+	public static void sum(){
+		int sumOfTwoNum = 10 + 10;
+		System.out.println(sumOfTwoNum);
 	}
 
 }
